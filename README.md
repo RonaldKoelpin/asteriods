10d278fc193fbea8f8154a74640aef9b98e0baa3
RonaldKoelpin <ronald.koelpin@gmail.com>
2025-12-07 01:32:26 +0100

    added readme, added to game loop

---
+++ README.md
@@ -0,0 +1,3 @@
+# asteriods
+
+Contains files for the [Boot.dev](https://www.boot.dev) asteriods project
--- main.py
+++ main.py
@@ -2,14 +2,14 @@ import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
 
-# initializing pygame
-pygame.init()
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
     while True:
         log_state()
         for event in pygame.event.get():
@@ -17,6 +17,7 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60)/1000
 
 if __name__ == "__main__":
     main()
